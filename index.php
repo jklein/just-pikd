6cc4d722a829bf47db75609f129adb3bac841ef4
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-03-04 19:35:53 -0500

    A little more framework

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
+    $controller = new \Pikd\Controller\Base();
+
+    $controller->render();
 });
 
 $app->run();
---
+++ src/Controller/Base.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pikd\Controller;
+
+class Base {
+    public function render() {
+        echo 'hello controller';
+    }
+}
