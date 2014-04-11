fa3d82bc3c3d37aced5c0797a21e82c35625b4da
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-04-11 21:24:00 +0000

    Removing stupid config file

--- config.rb
+++
@@ -1,8 +0,0 @@
-# Require any additional compass plugins here.
-require 'bootstrap-sass'
-
-http_path = '/'
-css_dir     = 'www/build/css'
-images_dir  = 'www/assets/images'
-sass_dir    = 'www/assets/scss'
-relative_assets = true
