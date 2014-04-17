27b6b87b3cdc0bfa52ae70d306a0eed37948a9a8
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-04-16 21:23:04 -0400

    Flash messages, footer, sessions

--- www/index.php
+++ www/index.php
@@ -1,6 +1,9 @@
 <?php
 require '../app/vendor/autoload.php';
 
+session_cache_limiter(false);
+session_start();
+
 $app = new \Slim\Slim([
     'view' => new \Slim\Views\Twig(),
 ]);
@@ -11,9 +14,17 @@ $view->parserOptions = array(
     'debug' => true,
 );
 
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+    new Twig_Extension_Debug(),
+);
+
 $app->get('/', function () use ($app) {
     $controller = new \Pikd\Controller\Base();
 
+    $app->flashNow('info', ['test', 'test 2']);
+    $app->flashNow('warning', 'test warning');
+    $app->flashNow('danger', ['test danger', 'test danger 2']);
     $app->render('index.twig', $controller->template_vars);
 });
 
