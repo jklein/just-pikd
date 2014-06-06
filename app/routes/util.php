baaa10107c213e51df03f8c2929bee5c05ca89e0
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-06-05 21:41:29 -0400

    Refactoring util page to test all of our services

--- app/routes/util.php
+++ app/routes/util.php
@@ -2,33 +2,24 @@
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
-$app->get('/util/memcached_test', function () use ($app) {
+$app->get('/util/testall', function () use ($app) {
+    // Memcached
     $memcached = new \Pikd\Cache\Memcached();
 
     $tmp_object = new stdClass;
     $tmp_object->str_attr = 'test';
     $tmp_object->int_attr = 123;
-
     $memcached->set('key2', $tmp_object, 10);
-    echo "Store data in the cache (data will expire in 10 seconds)<br/>\n";
+    $template_vars['memcached_value'] = $memcached->get('key2');
 
-    $get_result = $memcached->get('key2');
-    echo "Data from the cache:<br/>\n";
-
-    var_dump($get_result);
-
-});
-
-
-$app->get('/util/show_users', function () use ($app) {
+    // Database
     $conn = $app->connections->getRead('customer');
-    $results = $conn->fetchAll('SELECT * from customers');
-    \Pikd\Util::debug($results);
-});
+    $template_vars['users'] = $conn->fetchAll('SELECT * from customers limit 1');
 
-// Test monolog
-$app->get('/util/log', function () use ($app) {
+    // Monolog
     $log = new Logger('PIKD');
     $log->pushHandler(new StreamHandler('/var/log/testlog.log', Logger::WARNING));
 
@@ -36,4 +27,16 @@ $app->get('/util/log', function () use ($app) {
     $log->addWarning('This is a generic warning');
     $log->addError('This is a generic error');
     $log->addError('This is a generic error with some data', ['some' => 'data']);
+
+    // AMQP - RabbitMQ
+    $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+    $channel->queue_declare('hello', false, false, false, false);
+    $msg = new AMQPMessage('Hello World!');
+    $channel->basic_publish($msg, '', 'hello');
+    $template_vars['amqp'] = "Sent a message!";
+    $channel->close();
+    $connection->close();
+
+    $app->render('util.twig', $template_vars);
 });
