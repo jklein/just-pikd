f800e4bd03a0aed41ce4ecb0f5d047b560515bf8
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-03-06 13:48:30 -0500

    Fixing util page somewhat

--- app/routes/util.php
+++ app/routes/util.php
@@ -13,11 +13,13 @@ $app->get('/util/testall', function () use ($app) {
     $tmp_object->str_attr = 'test';
     $tmp_object->int_attr = 123;
     $memcached->set('key2', $tmp_object, 10);
-    $template_vars['memcached_value'] = $memcached->get('key2');
+    $template_vars['memcached_string'] = $memcached->get('key2')->str_attr;
+    $template_vars['memcached_int'] = $memcached->get('key2')->int_attr;
 
     // Database
     $conn = $app->connections->getRead('customer');
     $template_vars['users'] = $conn->fetchAll('SELECT * from customers limit 1');
+    d($template_vars['users']);
 
     // Monolog
     $log = new Logger('PIKD');
@@ -28,15 +30,5 @@ $app->get('/util/testall', function () use ($app) {
     $log->addError('This is a generic error');
     $log->addError('This is a generic error with some data', ['some' => 'data']);
 
-    // AMQP - RabbitMQ
-    $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
-    $channel = $connection->channel();
-    $channel->queue_declare('hello', false, false, false, false);
-    $msg = new AMQPMessage('Hello World!');
-    $channel->basic_publish($msg, '', 'hello');
-    $template_vars['amqp'] = "Sent a message!";
-    $channel->close();
-    $connection->close();
-
-    $app->render('util.twig', $template_vars);
+    $app->render('util', $template_vars);
 });
